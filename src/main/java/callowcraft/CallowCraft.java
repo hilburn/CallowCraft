f47a1dbf2f046310cb33663e00b3e69c5f151ad4
hilburn <charliepaterson89@gmail.com>
2014-12-12 14:51:03 +0000

    Updates

--- src/main/java/callowcraft/CallowCraft.java
+++ src/main/java/callowcraft/CallowCraft.java
@@ -2,9 +2,12 @@ package callowcraft;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,15 +18,16 @@ import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
-
 /**
  * CallowCraft Mod
  *
  * @author Charlie Paterson
- * @license BDaJ non-commercial care-free license. (https://github.com/hilburn/CallowCraft/blob/master/LICENSE.md)
+ * @license DBaJ non-commercial care-free license. (https://github.com/hilburn/CallowCraft/blob/master/LICENSE.md)
  **/
+
 @Mod(modid = "CallowCraft", name = "CallowCraft", version = "1.0.0")
 public class CallowCraft {
+    public static final String modID = "CallowCraft";
     public static double xpModifier = 1.0D;
     public static int radius = 1;
     public static int maxXPValue = 2048;
@@ -57,6 +61,13 @@ public class CallowCraft {
         config.save();
     }
 
+    @NetworkCheckHandler
+    public final boolean networkCheck(Map<String, String> remoteVersions, Side side)
+    {
+        if (side.isClient()) return true;
+        else return remoteVersions.containsKey(modID);
+    }
+
     public class xpHandler {
         private boolean all;
         private boolean noGroup;
@@ -70,23 +81,34 @@ public class CallowCraft {
         @SubscribeEvent
         public void xpHandler(EntityJoinWorldEvent e)
         {
-            if (!e.world.isRemote) {
-                if (e.entity instanceof EntityXPOrb) {
-                    if (this.all || e.world.rand.nextDouble() > xpModifier) {
-                        e.setCanceled(true);
-                        return;
-                    }
-                    if (noGroup) return;
-                    EntityXPOrb orb = (EntityXPOrb) e.entity;
-                    AxisAlignedBB axisAlignedBB = AxisAlignedBB.getBoundingBox(orb.posX - radius, orb.posY - radius, orb.posZ - radius, orb.posX + radius, orb.posY + radius, orb.posZ + radius);
-                    List<EntityXPOrb> nearbyOrbs = (List<EntityXPOrb>) e.world.getEntitiesWithinAABB(EntityXPOrb.class, axisAlignedBB);
-                    if (nearbyOrbs.isEmpty()) return;
-                    for (EntityXPOrb nearbyOrb : nearbyOrbs) {
-                        if (nearbyOrb.isDead || nearbyOrb.xpValue+orb.xpValue>maxXPValue) continue;
-                        orb.xpValue += nearbyOrb.getXpValue();
-                        e.world.removeEntity(nearbyOrb);
-                    }
+            if (e.entity instanceof EntityXPOrb) {
+                if (this.all || e.world.rand.nextDouble() > xpModifier) {
+                    e.setCanceled(true);
                     return;
+                }
+                if (noGroup) return;
+                EntityXPOrb orb = (EntityXPOrb) e.entity;
+                AxisAlignedBB axisAlignedBB = AxisAlignedBB.getBoundingBox(orb.posX - radius, orb.posY - radius, orb.posZ - radius, orb.posX + radius, orb.posY + radius, orb.posZ + radius);
+                List<EntityXPOrb> nearbyOrbs = (List<EntityXPOrb>) e.world.getEntitiesWithinAABB(EntityXPOrb.class, axisAlignedBB);
+                int nearby = 0;
+                for (EntityXPOrb nearbyOrb : nearbyOrbs) {
+                    if (nearbyOrb.isDead || nearbyOrb.xpValue+orb.xpValue>maxXPValue) continue;
+                    nearby++;
+                    orb.posX = nearbyOrb.posX;
+                    orb.posY = nearbyOrb.posY;
+                    orb.posZ = nearbyOrb.posZ;
+                    orb.motionX = orb.motionX*orb.xpValue + nearbyOrb.motionX*nearbyOrb.xpValue;
+                    orb.motionY = orb.motionY*orb.xpValue + nearbyOrb.motionY*nearbyOrb.xpValue;
+                    orb.motionZ = orb.motionZ*orb.xpValue + nearbyOrb.motionZ*nearbyOrb.xpValue;
+                    orb.xpValue += nearbyOrb.getXpValue();
+                    orb.motionX /= orb.xpValue;
+                    orb.motionY /= orb.xpValue;
+                    orb.motionZ /= orb.xpValue;
+                    e.world.removeEntity(nearbyOrb);
+                }
+                if (nearby>0) {
+                    e.setCanceled(true);
+                    e.world.spawnEntityInWorld(orb);
                 }
             }
         }
